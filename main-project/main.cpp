c2791c10530e21ceaee287feb7b0df1874fd7458
Vlad Pundalov <pundalov03@gmail.com>
2025-05-15 20:23:49 +0300

    Добавлены тестовые данные

--- main-project/main.cpp
+++ main-project/main.cpp
@@ -7,5 +7,6 @@ int main()
     cout << "Banking operation #7. GIT\n";
     cout << "Variant #7. Banking operation\n";
     cout << "Author: Vlad Pundalov\n";
+    cout << "Group: 24PInj\n";
     return 0;
 }
