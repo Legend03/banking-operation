0f570fee27eb8fa0753118a063d3090d18974de9
Pundalov Vladislav <79308953+Legend03@users.noreply.github.com>
2025-05-15 13:23:59 +0300

    Update main.cpp

---
+++ main-project/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Banking operation #7. GIT\n";
+    cout << "Variant #7. Banking operation\n";
+    cout << "Author: Vlad Pundalov\n";
+    return 0;
+}
