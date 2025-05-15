79924c07c449ef1642651446a61c6b37c2be5daf
Vlad Pundalov <pundalov03@gmail.com>
2025-05-15 14:37:21 +0300

    Добавлены структуры хранения данных

---
+++ main-project/banking-operation.h
@@ -0,0 +1,33 @@
+#ifndef BOOK_SUBSCRIPTION_H
+#define BOOK_SUBSCRIPTION_H
+
+#include "constants.h"
+#include <string>
+
+using namespace std;
+
+struct date
+{
+    int day;
+    int month;
+    int year;
+};
+
+struct time
+{
+    int hours;
+    int minutes;
+};
+
+struct book_subscription
+{
+    date date;
+    time time;
+    string kindOperation;
+    string bandAccount;
+    double amount;
+    string discription;
+    char title[MAX_STRING_SIZE];
+};
+
+#endif
